26ad7130c624327e42a67013d44efc61f9a0d222
antorahad <113632611+antorahad@users.noreply.github.com>
2023-08-03 20:40:51 +0600

    array

---
+++ 9.array.js
@@ -0,0 +1,11 @@
+let item = ['Book', 'Pen', 'Banana' , 'Mouse', 'Sunglass'];
+
+console.log (item);
+
+let number = [1, 2, 3, 5, 10, 15, 20];
+
+console.log (number);
+
+let friend = ['Fahim', 'Zubair', 'Palash', 'Fahad', 'Spondon', 'Jisan'];
+
+console.log (friend);
