1adf55fd52b7e273ed32d3172d860ad1592deb50
antorahad <113632611+antorahad@users.noreply.github.com>
2023-08-05 22:43:24 +0600

    odd array and odd array sum

--- 37.odd_array_odd_sum.js
+++ 37.odd_array_odd_sum.js
@@ -1,11 +1,38 @@
-let givenArray = [10, 3, 7, 8, 15, 12, 9];
+let myArray = [1, 3, 4, 8, 7, 49, 12, 11];
 
 function arrayCalc(numbers){
+
+    let oddNumbers = [];
+
     for(let i = 0; i < numbers.length; i++){
-        let arrayIndex = i;
-        let arrayElement = numbers[arrayIndex];
-        console.log(arrayIndex, arrayElement);
+        let index = i;
+
+        let element = numbers[index];
+
+        if(element % 2 == 1){
+            oddNumbers.push(element);
+        }
     }
+
+    return oddNumbers;
 }
 
-arrayCalc(givenArray);
+function oddArraySum(numbers){
+    let sum = 0;
+    
+    for(let i =0; i < numbers.length; i++){
+        let index = i;
+        let element = numbers[index];
+        if(element % 2 == 1){
+            sum = sum + element;
+        }
+    }
+
+    return sum;
+}
+
+let result = arrayCalc(myArray);
+
+let result2 = oddArraySum(myArray);
+
+console.log(result, result2);
