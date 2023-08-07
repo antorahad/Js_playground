ebd7e16a0763473d4e98f7adcac5b25da240bd8e
antorahad <113632611+antorahad@users.noreply.github.com>
2023-08-07 10:50:25 +0600

    array new

---
+++ 50.array_2.js
@@ -0,0 +1,20 @@
+let numbers = [1,2,3,4,5,6,7,8,9];
+
+// typeof array
+
+console.log(Array.isArray(numbers));
+
+
+// search for array
+
+console.log(numbers.includes(9));
+
+
+
+let newNumber = [10, 11, 12 , 13 , 14, 15];
+
+
+//concat of two array
+let allNumbers = numbers.concat(newNumber);
+
+console.log(allNumbers);
